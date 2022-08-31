07830947fa0e515baa226709fe3cfe0408c27a8b
khaled labeb <khaledtf19@gmail.com>
2022-08-31 15:00:15 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import helmet from "helmet";
-import * as csurf from "csurf";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(helmet());
-  app.use(csurf());
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
